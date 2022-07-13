88dde19f7e0a0f6a8828e6b420559cefc99cfbde
to-som-ja <davidkoso252123@gmail.com>
2022-07-13 11:21:25 +0200

    json import

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,22 +2,20 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import  tests  from './tests.json';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
   it('fail test', () => {
-    console.log("ahoj");
+    console.log('ahoj');
     console.log(environment.TEST1);
+    console.log(tests.TEST1);
     expect(false).toBeTruthy();
   });
 });
