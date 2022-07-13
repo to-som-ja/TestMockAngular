b60edc79843ebdbf29edaf9e6f915f5a8ef26f69
to-som-ja <davidkoso252123@gmail.com>
2022-07-13 12:53:52 +0200

    Created 4 test based on probalbilty

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,7 +2,6 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
-import  tests  from './tests.json';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -12,10 +11,28 @@ describe('AppComponent', () => {
     }).compileComponents();
   });
 
-  it('fail test', () => {
-    console.log('ahoj');
+  it('Test 1', () => {
     console.log(environment.TEST1);
-    console.log(tests.TEST1);
-    expect(false).toBeTruthy();
+    expect(Number.parseInt(environment.TEST1) / 100).toBeLessThan(
+      Math.random()
+    );
+  });
+  it('Test 2', () => {
+    console.log(environment.TEST2);
+    expect(Number.parseInt(environment.TEST2) / 100).toBeLessThan(
+      Math.random()
+    );
+  });
+  it('Test 3', () => {
+    console.log(environment.TEST3);
+    expect(Number.parseInt(environment.TEST3) / 100).toBeLessThan(
+      Math.random()
+    );
+  });
+  it('Test 4', () => {
+    console.log(environment.TEST4);
+    expect(Number.parseInt(environment.TEST4) / 100).toBeLessThan(
+      Math.random()
+    );
   });
 });
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,9 @@
 export const environment = {
   production: false,
   TEST1: '#{TEST1}#',
+  TEST2: '#{TEST2}#',
+  TEST3: '#{TEST3}#',
+  TEST4: '#{TEST4}#',
 };
 
 /*
